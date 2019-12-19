5c4e4f650c27d94f1cc3482614bc954a9172994f
dipanjana-dalui <31413248+dipanjana-dalui@users.noreply.github.com>
2019-12-19 14:06:52 -0500

    updated pseudocode for Gillespie Cpp

--- Pseudocode.c
+++ Pseudocode.c
@@ -29,6 +29,37 @@ int main( int count_of_command_line_arg, list_of_all_command_line_arg){
 
 	// generate random number
 	srand(use_computer_time)
+		
+	// start the simulation
+	do{ if current_time>next_time_pt_recorded {
+		double data_array[row_1][0] = current_t;
+	double data_array[row_1][1] = density_sp1;
+	double data_array[row-1][2] = density_sp2;
+	advance row++;
+	advance next_time_pt_recorded++;
+		}
+	   //rate of each event
+	   double event_1 = func1(parms1,parms2);
+	   double event2 = func2(parms1, parms2);
+	   
+	   // total event rate
+	   double total = event1 + event2;
+	   
+	   //determinne the next event, and update time
+	   
+	   
+	   
+	   //determinne the next event, and update event/populations
+	   
+	   
+	   
+	}
+	while(current_time < T_max && (population>0));
+	//record final state of data
+	double data_array[row_1][0] = current_t;
+	double data_array[row_1][1] = density_sp1;
+	double data_array[row-1][2] = density_sp2;
+	advance row++;
 
 
 }
